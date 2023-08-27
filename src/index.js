bb32a6a987ac751127bc2abbf6ba2cee6146ff2d
Aabid <hussain.aabid2002@gmail.com>
2023-08-27 12:57:40 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = express();
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const UserRepository = require('./repository/user-repository');
+
 const prepareAndStartServer = () => {
 
           app.use(bodyParser.json());
@@ -13,8 +15,11 @@ const prepareAndStartServer = () => {
 
           app.use('/api', apiRoutes);
 
-          app.listen(PORT, () => {
+          app.listen(PORT, async () => {
                     console.log(`Server Started on Port: ${PORT}`);
+                    // const repo = new UserRepository();
+                    // const response = await repo.getById(1);
+                    // console.log(response);
           });
 }
 
